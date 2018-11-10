3401239c2d3a1dc309764ed25c9036eb0930b784
Rok Kos <rocki5555@gmail.com>
2018-11-10 10:42:33 +0100

    Architecture: Draft for architecture and folder structure

---
+++ src/js/main.js
@@ -0,0 +1,29 @@
+
+function Start() {
+    // TODO: - Initiale buffers and drawing stuff
+    //       - Initialize physics
+    //       - Initialize game parameters
+    //       - Initiale controls
+    console.log("Start Game");
+}
+
+function Update() {
+    // TODO: - Get User input
+    //       - Calculate physics
+    //       - Move objects
+    //          - Move enemies
+    //          - Move character
+    //          - Move camera
+    //       - Draw stuff on screen
+    console.log("Update");
+}
+
+
+
+
+function main() {
+    Start();
+
+    setInterval(Update, 15);
+
+}
