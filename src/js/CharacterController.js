eabfa73710ecdcd2e301561c859d851bee866706
Sara Koljić <sk3545@student.uni-lj.si>
2018-11-11 18:47:14 +0000

    handle input1

--- src/js/CharacterController.js
+++ src/js/CharacterController.js
@@ -5,6 +5,44 @@ function InitInput() {
     DebugLog("Init User Input", kTagInput, "InitInput");
 }
 
+var currentlyPressedKeys = {};
+
+
+function initGL(canvas){
+    var gl = null;
+    try {
+        gl = canvas.getContext("webgl") || canvas.getContext("experimental-webgl");
+        gl.viewportWidth = cavas.width;
+        gl.viewportHeight = canvas.height;
+    }catch(e) {}
+    
+    if(!gl) {
+    alert("Unable to intialize WebGL.");
+    }
+    return gl;
+}
+
+
+
+function handleKeyDown(event) {
+    currentlyPressedKeys[event.keyCode] = true;
+}
+
+function handleKeyUp(event) {
+    currentlyPressedKeys[event.keyCode] = false;
+}
+
 function HandleInput() {
-    //TODO: handle input from user
+    if(currentlyPressedKeys[33]) //page up
+        positionZ -= 0.05;
+    if(currentlyPressedKeys[34]) //page down
+        positionZ += 0.05;
+    if(currentlyPressedKeys[37]) //left
+        rotationVelocityY -= 1;
+    if(currentlyPressedKeys[39]) //right
+        rotationVelocityY += 1;
+    if(currentlyPressedKeys[38]) //up
+        rotationVelocityX -= 1;
+    if(currentlyPressedKeys[40]) //down
+        rotationVelocityX += 1;
 }
