a3de8531b1a747ef1a97976cd134767e33eef5f8
Rok Kos <rocki5555@gmail.com>
2018-12-25 20:31:18 +0100

    Unity Project Setup and draft for Bullet Pool

---
+++ pijani-ivan-unity/Assets/Scripts/BulletPoolController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletPoolController : MonoBehaviour
+{
+    [SerializeField] BulletController prefab;
+    [SerializeField] GameObject poolSceneParent;
+
+    private Queue<BulletController> pool;
+    private const int kBulletsNumber = 100;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pool = new Queue<BulletController>(kBulletsNumber);
+        for (int i = 0; i < kBulletsNumber; ++i) {
+            BulletController bullet = (BulletController)Instantiate(prefab, poolSceneParent.transform);
+            pool.Enqueue(bullet);
+        }
+    }
+}
