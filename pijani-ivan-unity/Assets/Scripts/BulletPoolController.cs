e956f5248baf648afd8f53ccb506c122dfd1ee73
Rok Kos <rocki5555@gmail.com>
2018-12-26 16:57:58 +0100

    Fixed issue with jumping character

--- pijani-ivan-unity/Assets/Scripts/BulletPoolController.cs
+++ pijani-ivan-unity/Assets/Scripts/BulletPoolController.cs
@@ -10,15 +10,16 @@ public class BulletPoolController : MonoBehaviour
 
     private Queue<BulletController> pool;
     private const int kBulletsNumber = 50;
+    private Vector3 farAwayVector = new Vector3(10000, 10000, 10000);
 
     // Start is called before the first frame update
     void Start()
     {
         pool = new Queue<BulletController>(kBulletsNumber);
         for (int i = 0; i < kBulletsNumber; ++i) {
-            BulletController bullet = (BulletController)Instantiate(prefab);
+            BulletController bullet = (BulletController)Instantiate(prefab, farAwayVector, Quaternion.identity, poolSceneParent);
             bullet.name = System.String.Format("Bullet {0:D2}", i);
-            ResetBullet(bullet, true, poolSceneParent);
+            bullet.gameObject.SetActive(false);
             bullet.SetBulletPoolController(this);
             pool.Enqueue(bullet);
         }
@@ -37,7 +38,12 @@ public class BulletPoolController : MonoBehaviour
 
     private void ResetBullet(BulletController bullet, bool activate, Transform parent) {
         bullet.transform.SetParent(parent);
-        bullet.transform.position = Vector3.zero;
+        if (!activate) {
+            bullet.transform.position = farAwayVector;
+        } else {
+            bullet.transform.position = Vector3.zero;
+        }
+        
         bullet.gameObject.SetActive(activate);
     }
 }
