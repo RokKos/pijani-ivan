782630023267282a37d55686ab6753bb68cc5ca0
Rok Kos <rocki5555@gmail.com>
2018-12-26 14:55:40 +0100

    Camera is following player and also Medved imported

---
+++ pijani-ivan-unity/Assets/Scripts/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] Transform characterTransform;
+    [SerializeField] Vector3 Offset;
+
+    // Update is called once per frame
+    void Update(){
+        Vector3 pos = characterTransform.position;
+        pos -= Offset;
+        transform.position = pos;
+
+        transform.rotation = characterTransform.rotation;
+        
+    }
+}
