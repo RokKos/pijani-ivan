2789cdf4e416c5d77278573a5c28d090d523a46c
Rok Kos <rocki5555@gmail.com>
2019-01-02 19:28:17 +0100

    Fixing animation of the key. Now it rotates relative to its position

---
+++ pijani-ivan-unity/Assets/Scripts/KeyController.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyController : MonoBehaviour {
+    private void OnTriggerEnter(Collider other) {
+
+        Destroy(this.gameObject);
+    }
+}
