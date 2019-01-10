5680766df60ac0e23d0e74bf7867f1a26d86696b
SaraKoljic <sk3545@student.uni-lj.si>
2019-01-10 16:05:54 +0100

    sara ui

---
+++ pijani-ivan-unity/Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+
+    public void changeToScene(int SceneToChangeTo)
+    {
+        SceneManager.LoadScene(SceneToChangeTo);
+    }
+   
+}
